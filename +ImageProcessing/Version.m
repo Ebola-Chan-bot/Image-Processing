41062c3f998f0c35e4c0a66d90b1d20cc181241d
埃博拉酱 <vhtmfu@outlook.com>
2023-04-04 16:44:42 +0800

    v3.4.1

--- +ImageProcessing/Version.m
+++ +ImageProcessing/Version.m
@@ -1,3 +1,13 @@
 function V = Version
-V.Me='3.4.0';
+V.Me='v3.4.1';
 V.MATLAB='R2022b';
+persistent NewVersion
+try
+	if isempty(NewVersion)
+		NewVersion=TextAnalytics.CheckUpdateFromGitHub('https://github.com/Silver-Fang/Image-Processing/releases','埃博拉酱的图像处理工具箱',V.Me);
+	end
+catch ME
+	if ME.identifier~="MATLAB:undefinedVarOrClass"
+		ME.rethrow;
+	end
+end
